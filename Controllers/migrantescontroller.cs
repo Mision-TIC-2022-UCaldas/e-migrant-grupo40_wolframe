627622e7ca25b263086205d22e01640e1ec4c226
ingemateo30 <msalazar5@udi.edu.co>
2021-11-26 18:08:42 -0500

    Crud migrante

---
+++ Controllers/migrantescontroller.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using proyecto.Data;
+using proyecto.Models;
+using System.Collections.Generic;
+
+namespace proyecto.Controllers
+{
+    public class migrantescontroller : Controller
+
+    {
+        private readonly ApplicationDbContext _context;
+
+        public migrantescontroller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<migrantes> lista_migrantes = _context.migrantes;
+            return View(lista_migrantes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(migrantes migrantes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.migrantes.Add(migrantes);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "se ha registrado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var migrante = _context.migrantes.Find(id);
+            
+            if (migrante == null)
+            {
+                return NotFound();
+            }
+            return View(migrante);
+        }
+        [HttpPost]
+        public IActionResult Edit(migrantes migrantes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.migrantes.Update(migrantes);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "se ha editado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var migrante = _context.migrantes.Find(id);
+
+            if (migrante == null)
+            {
+                return NotFound();
+            }
+            return View(migrante);
+        }
+        [HttpPost]
+        public IActionResult Deletemigrante(int? id)
+        {
+            var migrante = _context.migrantes.Find(id);
+
+            if (migrante == null)
+            {
+                return NotFound();
+            }
+            _context.migrantes.Remove(migrante);
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "se ha eliminado correctamente";
+            return RedirectToAction("Index");
+            
+
+
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proyecto.Models;
+
+namespace proyecto.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {
+
+        }
+
+        public DbSet<migrantes> migrantes { get; set; }
+    }
+}
