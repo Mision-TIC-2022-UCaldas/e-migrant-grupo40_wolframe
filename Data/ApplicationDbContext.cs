5071857f70943700f34825947f02a7f898b60dad
ingemateo30 <msalazar5@udi.edu.co>
2021-11-27 10:33:27 -0500

    retos de login

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using login2.Models;
+
+namespace login2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<login2.Models.migrantes> migrantes { get; set; }
+    }
+}
---
+++ Models/migrantes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace login2.Models
+{
+    public class migrantes
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellidos { get; set; }
+        [Required]
+        public string Tipodoc { get; set; }
+        [Required]
+        public string Documento { get; set; }
+        [Required]
+        public string Pais { get; set; }
+        [Required]
+        public DateTime Fecha_nacimiento { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public string Situacionlaboral { get; set; }
+    }
+}
